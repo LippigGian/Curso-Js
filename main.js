a6bd8167028fb34bfd3a356dc08a5ac8a43352d5
Gianfranco Lippi <gian.lippi@yahoo.com.ar>
2023-01-12 16:33:42 -0300

    Segunda pre-entrega Lippi

--- main.js
+++ main.js
@@ -1,91 +1,95 @@
-//funcion de calculadora de divisas
-const calculadoraDivisas = () => {
-  let monto = 0;
-  let precioDolar = 330;
-  let precioEuro = 355;
-  let precioReal = 33;
-  let seguirConvirtiendo = false;
-  let montoTotal = 0;
-  let resultado = 0;
-
-  do {
-    monto = parseInt(prompt("Ingrese que monto de AR$ desea convertir: "));
-
-    const cantidadValidada = validarCantidad(monto); //invoco a la funcion y le paso el parametro que quiero pasarle a la funcion (es buena practica usar const y no let)
-    //ademas le asigno el valor de la funcion a la variable "cantidadValidada"
-
-    tipoMoneda = prompt("¿A que divisa desea convertir? ¿Euro, Dolar o Reales?").toUpperCase();
-    console.log(tipoMoneda); //para chequear el valor de tipoMoneda
-
-    const resultado = calcular(cantidadValidada, tipoMoneda, precioDolar, precioEuro, precioReal);
-    console.log(resultado); //para chequear el valor de resultado
-
-    switch (tipoMoneda) {
-      case "EURO":
-        alert("Seleccionaste Euro. Tu cambio es de " + resultado +" Euros");
-        break;
-      case "DOLAR":
-        alert("Seleccionaste Dolar. Tu cambio es de " + resultado +" Dolares");
-        break;
-      case "REALES":
-        alert("Seleccionaste Reales. Tu cambio es de " + resultado +" Reales");
-        break;
-
-      default:     
-            monto = 0;
-        break;
-
-    }
-
-    montoTotal += monto;
-    console.log(montoTotal); // para chequear el valor de montoTotal
-
-    if (tipoMoneda != "EURO" || tipoMoneda != "DOLAR" || tipoMoneda != "REALES" ) {
-        seguirConvirtiendo = confirm("Opción inválida. ¿Desea convertir otra divisa?");
+/*necesito crear una funcion que inicie todo el ciclo, en la cual voy a preguntar en que orden quiero mostrar las divisas*/
+const inicioCiclo = () =>{
+    const inicio = confirm("¿Desea ordenar las divisas de la mas barata a la mas cara?")
+    if (inicio)
+    {
+        ordenarMenorMayor()
     }
     else
     {
-    seguirConvirtiendo = confirm("¿Desea convertir otra divisa?");
+        ordenarMayorMenor()
     }
+}
 
-  } while (seguirConvirtiendo);
-  alert("Usted ha cambiado un total de " + montoTotal + " pesos");
+// Array para el carrito de compras
+const carrito = []
 
-  function validarCantidad(monto) {
-    while (Number.isNaN(monto) || monto === 0) {
-      if (monto !== 0) {
-        alert("Debe agregar un número.");
-      } else {
-        alert("Debe agregar un número distinto de cero.");
-      }
-      monto = parseInt(prompt("Ingrese el monto que desea convertir sus AR$"));
-    }
-    return monto; /*return monto para que vuelva a la linea 40 donde la funcion es invocada*/
-  }
+/*Funcion para ordenar de menor a mayor utilizando SORT*/
+const ordenarMenorMayor = () => {
 
-
-  function calcular(cantidadValidada, tipoMoneda, precioDolar, precioEuro, precioReal) {
-    switch (tipoMoneda) {
-      case "EURO":
-        resultado = cantidadValidada / precioEuro;
-        return resultado;
-
-        break;
-
-      case "REALES":
-        resultado = cantidadValidada / precioReal;
-        return resultado;
-        break;
-
-      case "DOLAR":
-        resultado = cantidadValidada / precioDolar;
-        return resultado;
-        break;
-
-      default:
-        break;
-    }
-  }
+    /*sort para ordenar el array*/
+    monedas.sort((a,b) => a.valor - b.valor)
+    mostrarListaOrdenada()
 };
 
-calculadoraDivisas();
+/*Funcion para ordenar de mayor a menor utilizando SORT*/
+const ordenarMayorMenor = () => {
+    /*sort para ordenar el array*/
+    monedas.sort((a,b) => b.valor - a.valor)
+    mostrarListaOrdenada()
+};
+
+/*con esta funcion muestro el array ya ordenado*/
+const mostrarListaOrdenada = () => {
+    const mostrar = monedas.map(moneda => {
+        return moneda.nombre + " $"+ moneda.valor+ "\n"})
+    alert(mostrar)
+    tipoMoneda()
+}
+
+
+/*inicio del programa en si*/
+const tipoMoneda = () =>{
+do {
+    const monedaTipo = prompt("¿A que divisa desea convertir sus pesos? ¿Dolar, Euro, Real, Libra o Bitcoin?")
+
+    const monedaMonto = parseFloat(prompt("¿Qué cantidad de pesos desea convertir?"))
+
+    const moneda = monedas.find(moneda => moneda.nombre.toLowerCase() === monedaTipo.toLowerCase())
+    console.log(monedaTipo)
+
+    if (moneda) {
+        alert("Usted recibe "+monedaMonto/moneda.valor+" de "+moneda.nombre)
+        
+        agregarAlCarrito(moneda, moneda.id, monedaMonto)
+
+    } else {
+        alert('La moneda seleccionada es invalida')
+    }
+    confirmar = confirm('¿Desea realizar otra operación?')
+
+} while (confirmar);
+
+confirmarCompra()
+console.log(monedas)
+}
+/*Funcion para agregar al "carrito" las monedas y tipo que lleva*/
+    const agregarAlCarrito = (moneda, monedaId, monedaMonto) => {
+
+        const productoRepetido = carrito.find(moneda => moneda.id === monedaId)
+        if (!productoRepetido) {
+            moneda.cantidad += monedaMonto/moneda.valor;
+            carrito.push(moneda)
+        } else {
+            productoRepetido.cantidad += monedaMonto/moneda.valor;
+        }
+    };
+/*Checkout para mostrar que tipo y cantidad de monedas lleva*/
+    const confirmarCompra = () => {
+        const listaProductos = carrito.map(monedas => {
+        return '-Usted ha cambiado '+monedas.cantidad +' de: '+monedas.nombre
+        })
+        console.log(listaProductos) 
+
+        if(listaProductos.length === 0)
+        {
+            alert("No seleccionaste ninguna opción válida")
+        }
+        else
+        {
+            alert('Checkout: '+'\n\n'+listaProductos.join('\n'))
+        }
+}
+
+/*inicioCiclo va al o ultimo de todo*/
+inicioCiclo()
